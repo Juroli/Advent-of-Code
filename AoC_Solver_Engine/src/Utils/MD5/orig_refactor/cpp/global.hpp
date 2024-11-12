f0ef61a3aa6a4e6835cd3ccf7a27a47d96ed6bfb
JR <jr@mail.com>
2024-11-12 17:40:29 +0100

    Refactored MD5 C++

--- AoC_Solver_Engine/src/Utils/MD5/orig_refactor/cpp/global.hpp
+++ AoC_Solver_Engine/src/Utils/MD5/orig_refactor/cpp/global.hpp
@@ -1,13 +1,13 @@
 
 /* GLOBAL.H - RSAREF types and constants
- */
+*/
 
-/* POINTER defines a generic pointer type */
+ /* POINTER defines a generic pointer type */
 typedef unsigned char* POINTER;
 
 /* UINT2 defines a two byte word */
-typedef unsigned short int UINT2;
+//typedef unsigned short int UINT2;
 
 /* UINT4 defines a four byte word */
-typedef unsigned long int UINT4;
+//typedef unsigned long int uint32_t;
 
