d2a2e27310e1c7012977a9110e3f03ecd994ffef
JR <jr@mail.com>
2024-11-13 16:54:29 +0100

    Removed useless comments from MD5.

--- AoC_Solver_Engine/src/Utils/MD5/TMD5.cpp
+++ AoC_Solver_Engine/src/Utils/MD5/TMD5.cpp
@@ -7,7 +7,6 @@
 
 
 //char TMD5::PADDING[64] = {
-//std::array<char, 64> TMD5::PADDING = {
 const std::vector<uint8_t> PADDING = {
 	0x80, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
 	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
@@ -15,31 +14,30 @@ const std::vector<uint8_t> PADDING = {
 	};
 
 
-constexpr uint32_t S11 = 7;						//	#define S11 7
-constexpr uint32_t S12 = 12;						//	#define S12 12
-constexpr uint32_t S13 = 17;						//	#define S13 17
-constexpr uint32_t S14 = 22;						//	#define S14 22
-constexpr uint32_t S21 = 5;						//	#define S21 5
-constexpr uint32_t S22 = 9;						//	#define S22 9
-constexpr uint32_t S23 = 14;						//	#define S23 14
-constexpr uint32_t S24 = 20;						//	#define S24 20
-constexpr uint32_t S31 = 4;						//	#define S31 4
-constexpr uint32_t S32 = 11;						//	#define S32 11
-constexpr uint32_t S33 = 16;						//	#define S33 16
-constexpr uint32_t S34 = 23;						//	#define S34 23
-constexpr uint32_t S41 = 6;						//	#define S41 6
-constexpr uint32_t S42 = 10;						//	#define S42 10
-constexpr uint32_t S43 = 15;						//	#define S43 15
-constexpr uint32_t S44 = 21;						//	#define S44 21
+constexpr uint32_t S11 = 7;
+constexpr uint32_t S12 = 12;
+constexpr uint32_t S13 = 17;
+constexpr uint32_t S14 = 22;
+constexpr uint32_t S21 = 5;
+constexpr uint32_t S22 = 9;
+constexpr uint32_t S23 = 14;
+constexpr uint32_t S24 = 20;
+constexpr uint32_t S31 = 4;
+constexpr uint32_t S32 = 11;
+constexpr uint32_t S33 = 16;
+constexpr uint32_t S34 = 23;
+constexpr uint32_t S41 = 6;
+constexpr uint32_t S42 = 10;
+constexpr uint32_t S43 = 15;
+constexpr uint32_t S44 = 21;
 
 static constexpr uint32_t _F( uint32_t x, uint32_t y, uint32_t z ) { return ((x & y) | ((~x) & z)); }
 static constexpr uint32_t _G( uint32_t x, uint32_t y, uint32_t z ) { return (x & z) | (y & (~z)); }
 static constexpr uint32_t _H( uint32_t x, uint32_t y, uint32_t z ) { return (x ^ y ^ z); }
 static constexpr uint32_t _I( uint32_t x, uint32_t y, uint32_t z ) { return (y ^ (x | (~z))); }
 
-// ROTATE_LEFT rotates x left n bits.
+
 static constexpr uint32_t _BitRotation_Left( uint32_t x, int n ) { return ((x << n) | (x >> (32 - n))); }
-//std::uint32_t _BitRotation_Left(std::uint32_t x, int n)					{ return _rotl(x, n); }
 
 
 static constexpr uint32_t _FF( uint32_t a, uint32_t b, uint32_t c, uint32_t d, uint32_t x, uint32_t s, uint32_t ac )
@@ -70,54 +68,24 @@ static constexpr uint32_t _II( uint32_t a, uint32_t b, uint32_t c, uint32_t d, u
 TMD5::TMD5()
 	: m_bytecount( 0 )
 	, m_MD5 { 0x67452301, 0xefcdab89, 0x98badcfe, 0x10325476 }
-//: m_digest(reinterpret_cast<unsigned char *>(m_MD5))
 {
-	//_MD5Init();
 }
-
-// TMD5::TMD5(std::istream const &pin)
-// {
-// 	_MD5Init();
-// }
 
 TMD5::TMD5( const std::string& pin )
 	: m_bytecount( 0 )
 	, m_MD5{ 0x67452301, 0xefcdab89, 0x98badcfe, 0x10325476 }
-
-//: m_digest(reinterpret_cast<unsigned char *>(m_MD5))
 {
-	//_MD5Init();
-	//_MD5Update(buffer.data(), buffer.size());
-	//_MD5Update(pin.data(), pin.size());
-	//_MD5Update(pin);		//.data());
+
 	i_MD5Update( std::vector<uint8_t>( pin.begin(), pin.end() ) );
 
 	i_MD5Final();
-
 }
 
 
-TMD5::~TMD5()
-{
-	// 	for(auto &curr : m_count)
-	// 	{
-	// 		curr = 0;
-	// 	}
-	// 	for(auto &curr : m_buffer)
-	// 	{
-	// 		curr = 0;
-	// 	}
-	// 
-	// 	for(auto &curr : m_MD5)
-	// 	{
-	// 		curr = 0;
-	// 	}
-}
 
 std::string TMD5::String() const
 {
 	std::string result( 32, '+' );
-	//result.resize(32);
 
 	const char lookup[16] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f' };
 
@@ -138,199 +106,24 @@ std::string TMD5::String() const
 		
 	}
 
-
-	//const void* tmp_ptr = m_MD5;
-	//const auto digest = static_cast<const uint8_t*>(tmp_ptr);
-
-	//for (int i = 0, j = 0; i < 16; ++i)
-	//{
-	//	//char tmp1 = m_digest[i];
-	//	//char tmp2 = (tmp1 >> 4) & 0x0F;
-	//	//char tmp3 = tmp1 & 0x0F;
-
-	//	result[j++] = lookup[(digest[i] >> 4) & 0x0F];
-	//	result[j++] = lookup[digest[i] & 0x0F];
-	//}
-
 	return result;
 }
 
-//void TMD5::_CalcMD5()
-//{
-//
-//}
 
-// constexpr std::uint32_t F(std::uint32_t x, std::uint32_t y, std::uint32_t z)
-// {
-// 	return ((x & y) | ((~x) & z));
-// }
-// constexpr std::uint32_t G(std::uint32_t x, std::uint32_t y, std::uint32_t z)
-// {
-// 	return (x & z) | (y & (~z));
-// }
-// constexpr std::uint32_t H(std::uint32_t x, std::uint32_t y, std::uint32_t z)
-// {
-// 	return (x ^ y ^ z);
-// }
-// constexpr std::uint32_t I(std::uint32_t x, std::uint32_t y, std::uint32_t z)
-// {
-// 	return ( y ^ (x | (~z)));
-// }
-// 
-// /* ROTATE_LEFT rotates x left n bits.
-// */
-// //#define ROTATE_LEFT(x, n) (((x) << (n)) | ((x) >> (32-(n))))
-// constexpr std::uint32_t BitRotation_Left(std::uint32_t x, int n)
-// {
-// 	return ((x << n) | (x >> (32 - n)));
-// }
-
-/* FF, GG, HH, and II transformations for rounds 1, 2, 3, and 4.
-Rotation is separate from addition to prevent re-computation.
-*/
-// #define FF(a, b, c, d, x, s, ac) { \
-//  (a) += F ((b), (c), (d)) + (x) + (uint32_t)(ac); \
-//  (a) = BitRotation_Left ((a), (s)); \
-//  (a) += (b); \
-// }
-// #define GG(a, b, c, d, x, s, ac) { \
-//  (a) += G ((b), (c), (d)) + (x) + (uint32_t)(ac); \
-//  (a) = BitRotation_Left ((a), (s)); \
-//  (a) += (b); \
-//   }
-// #define HH(a, b, c, d, x, s, ac) { \
-//  (a) += H ((b), (c), (d)) + (x) + (uint32_t)(ac); \
-//  (a) = BitRotation_Left ((a), (s)); \
-//  (a) += (b); \
-//   }
-// #define II(a, b, c, d, x, s, ac) { \
-//  (a) += I ((b), (c), (d)) + (x) + (uint32_t)(ac); \
-//  (a) = BitRotation_Left ((a), (s)); \
-//  (a) += (b); \
-//   }
-
-// constexpr std::uint32_t FF(std::uint32_t a, std::uint32_t b, std::uint32_t c, std::uint32_t d,
-// 	std::uint32_t x, int s, std::uint32_t ac)
-// {
-// 	return BitRotation_Left(a + F(b, c, d) + x + ac, s) + b;
-// }
-// constexpr std::uint32_t GG(std::uint32_t a, std::uint32_t b, std::uint32_t c, std::uint32_t d,
-// 	std::uint32_t x, int s, std::uint32_t ac)
-// {
-// 	return BitRotation_Left(a + G(b, c, d) + x + ac, s) + b;
-// }
-// 
-// constexpr std::uint32_t HH(std::uint32_t a, std::uint32_t b, std::uint32_t c, std::uint32_t d,
-// 	std::uint32_t x, int s, std::uint32_t ac)
-// {
-// 	return BitRotation_Left(a + H(b, c, d) + x + ac, s) + b;
-// }
-// constexpr std::uint32_t II(std::uint32_t a, std::uint32_t b, std::uint32_t c, std::uint32_t d,
-// 	std::uint32_t x, int s, std::uint32_t ac)
-// {
-// 	return BitRotation_Left(a + I(b, c, d) + x + ac, s) + b;
-// }
-
-
-/* MD5 initialization. Begins an MD5 operation, writing a new context.
-*/
-//void TMD5::_MD5Init()		/* context */
-//{
-//	//m_count[0] = m_count[1] = 0;
-//	m_bytecount = 0;
-//	/* Load magic initialization constants.
-//	*/
-//	m_MD5 = { 0x67452301, 0xefcdab89, 0x98badcfe, 0x10325476 };
-//
-//	//m_MD5[0] = 0x67452301;
-//	//m_MD5[1] = 0xefcdab89;
-//	//m_MD5[2] = 0x98badcfe;
-//	//m_MD5[3] = 0x10325476;
-//}
-
-/* MD5 block update operation. Continues an MD5 message-digest
-operation, processing another message block, and updating the
-context.
-*/
-//void TMD5::_MD5Update (char const *input, uint32_t inputLen)
 
 void TMD5::i_MD5Update( const std::vector<uint8_t>& ainput )
 {
-	//_MD5Update(pinput, pinput.size());
 	i_MD5Update( ainput.begin(), ainput.end() );
 }
 
-// void TMD5::_O_MD5Update (std::vector<char> const &pinput, uint32_t inputLen)
-// {
-// 	//uint32_t i;			//, index;		//, partLen;
-// 
-// 	/* Compute number of bytes mod 64 */
-// 	// index = (m_count[0] >> 3) & 0x3F;
-// 	//index = (m_count[0] / 8) % 64;
-// 	uint32_t index = (m_bitcount / 8) % 64; 
-// 
-// 	/* Update number of bits */
-// 	// 	if ((m_count[0] += (inputLen << 3))
-// 	// 		< (inputLen << 3))
-// 	// 	{
-// 	// 		m_count[1]++;
-// 	// 	}
-// 	// 	m_count[1] += (inputLen >> 29);
-// 	m_bitcount += inputLen * 8;
-// 
-// 	uint32_t partLen = 64 - index;
-// 
-// 	uint32_t i = 0;
-// 
-// 	/* Transform as many times as possible.
-// 	*/
-// 	if (inputLen >= partLen)
-// 	{
-// 		_MD5_memcpy(&m_buffer[index], pinput.data(), partLen);
-// 		_MD5Transform (m_buffer);
-// 
-// 		for (i = partLen; i + 63 < inputLen; i += 64)
-// 		{
-// 			_MD5Transform (&pinput.data()[i]);
-// 		}
-// 
-// 		index = 0;
-// 	}
-// 	// 	else
-// 	// 	{
-// 	// 		i = 0;
-// 	// 	}
-// 
-// 	/* Buffer remaining input */
-// 	_MD5_memcpy(&m_buffer[index], &pinput.data()[i], inputLen-i);
-// }
 
 
 void TMD5::i_MD5Update( std::vector<uint8_t>::const_iterator astart
 	, const std::vector<uint8_t>::const_iterator& aend )
 {
-	//uint32_t i;			//, index;		//, partLen;
 
-	/* Compute number of bytes mod 64 */
-	// index = (m_count[0] >> 3) & 0x3F;
-	//index = (m_count[0] / 8) % 64;
 	uint32_t index = m_bytecount % 64;
 
-	/* Update number of bits */
-	// 	if ((m_count[0] += (inputLen << 3))
-	// 		< (inputLen << 3))
-	// 	{
-	// 		m_count[1]++;
-	// 	}
-	// 	m_count[1] += (inputLen >> 29);
-
-	//uint32_t inputLen = 0;
-
-	//uint8_t block[64];
-
-	//	uint32_t partLen = 64 - index;
-
-	//	uint32_t i = 0;
 
 	while (astart < aend)
 	{
@@ -338,7 +131,6 @@ void TMD5::i_MD5Update( std::vector<uint8_t>::const_iterator astart
 
 		++index;
 		++astart;
-		//++inputLen;
 		++m_bytecount;
 
 		if (index >= 64)
@@ -349,29 +141,6 @@ void TMD5::i_MD5Update( std::vector<uint8_t>::const_iterator astart
 		}
 	}
 
-	/* Transform as many times as possible.
-	*/
-	// 	if (inputLen >= partLen)
-	// 	{
-	// 		_MD5_memcpy(&m_buffer[index], pinput.data(), partLen);
-	// 		_MD5Transform (m_buffer);
-	// 
-	// 		for (i = partLen; i + 63 < inputLen; i += 64)
-	// 		{
-	// 			_MD5Transform (&pinput.data()[i]);
-	// 		}
-	// 
-	// 		index = 0;
-	// 	}
-	// 	// 	else
-	// 	// 	{
-	// 	// 		i = 0;
-	// 	// 	}
-	// 
-	// 	/* Buffer remaining input */
-	// 	_MD5_memcpy(&m_buffer[index], &pinput.data()[i], inputLen-i);
-
-	//m_bytecount += inputLen;		// *8;
 }
 
 
@@ -381,54 +150,28 @@ the message digest and zeroizing the context.
 */
 void TMD5::i_MD5Final()
 {
-	//char bits[8];
-	// std::string bits(8, '0');
-//	unsigned char const *bits_2 = reinterpret_cast<unsigned char *>(m_count);
-
-	//uint32_t index, padLen;
-
 	/* Save number of bits */
-	//_Encode (bits, m_count, 8);
 	const std::vector<uint8_t> bits = i_Encode( m_bytecount * 8 );
 
-	/* Pad out to 56 mod 64.
-	*/
-	//index = (m_count[0] >> 3) & 0x3f;
+	/* Pad out to 56 mod 64. */
 	const uint32_t index = m_bytecount % 64;
 	const uint32_t padLen = (index < 56) ? (56 - index) : (120 - index);
-	//_MD5Update (PADDING, padLen);
 	i_MD5Update( PADDING.begin(), PADDING.begin() + padLen );
 
 	/* Append length (before padding) */
-	//_MD5Update (bits, 8);
 	i_MD5Update( bits.begin(), bits.begin() + 8 );
-
-	/* Store state in digest */
-//	_Encode (m_digest, m_MD5, 16);
-
-	/* Zeroize sensitive information.
-	*/
-	//_MD5_memset ((POINTER)context, 0, sizeof (*context));
 
 }
 
-/* MD5 basic transformation. Transforms state based on block.
-*/
-//void TMD5::_MD5Transform (uint32_t state[4], std::array<char, 64> block)	// const  char const block[64])
-//void TMD5::i_MD5Transform( const uint8_t block[64] )
+/* MD5 basic transformation. Transforms state based on block. */
+
 void TMD5::i_MD5Transform( const uint32_t ax[16] )
-//void TMD5::_MD5Transform (std::array<char, 64> block)
 {
-	uint32_t a = m_MD5[0];			//state[0];
-	uint32_t b = m_MD5[1];			//state[1];
-	uint32_t c = m_MD5[2];			//state[2];
-	uint32_t d = m_MD5[3];			//state[3];
+	uint32_t a = m_MD5[0];
+	uint32_t b = m_MD5[1];
+	uint32_t c = m_MD5[2];
+	uint32_t d = m_MD5[3];
 
-	//std::uint32_t x[16];
-	//const std::uint32_t* x = reinterpret_cast<std::uint32_t const*>(block);
-	//std::uint32_t const *x = reinterpret_cast<std::uint32_t const *>(block.data());
-
-//	_Decode (x, block, 64);
 
 	/* Round 1 */
 	a = _FF( a, b, c, d, ax[0], S11, 0xd76aa478 ); /* 1 */
@@ -502,52 +245,18 @@ void TMD5::i_MD5Transform( const uint32_t ax[16] )
 	c = _II( c, d, a, b, ax[2], S43, 0x2ad7d2bb ); /* 63 */
 	b = _II( b, c, d, a, ax[9], S44, 0xeb86d391 ); /* 64 */
 
-	m_MD5[0] += a;				// 	state[0] += a;
-	m_MD5[1] += b;				// 	state[1] += b;
-	m_MD5[2] += c;				// 	state[2] += c;
-	m_MD5[3] += d;				// 	state[3] += d;
+	m_MD5[0] += a;
+	m_MD5[1] += b;
+	m_MD5[2] += c;
+	m_MD5[3] += d;
 
-	/* Zeroize sensitive information.
-	*/
-	// 	for(auto &curr : x)
-	// 	{
-	// 		curr = 0;
-	// 	}
-		//_MD5_memset (x, 0, sizeof (x));
 }
 
 /* Encodes input (UINT4) into output (unsigned char). Assumes len is
 a multiple of 4.
 */
-// void TMD5::_Encode (char *output, std::uint32_t const *input, uint32_t len)
-// {
-// 	uint32_t i, j;
-// 
-// 	for (i = 0, j = 0; j < len; i++, j += 4) {
-// 		output[j] = input[i] & 0xff;
-// 		output[j+1] = (input[i] >> 8) & 0xff;
-// 		output[j+2] = (input[i] >> 16) & 0xff;
-// 		output[j+3] = (input[i] >> 24) & 0xff;
-// 	}
-// }
-
 std::vector<uint8_t> TMD5::i_Encode( const std::uint64_t& input )
 {
-	//std::vector<uint8_t> result( sizeof( input ), 0u );
-	////uint32_t i, j;
-
-	//for (auto i = 0u, j = 0u; i < sizeof( input ); ++i, j += 8)
-	//{
-	//	result[i] = (input >> j) & 0xff;
-
-	//	// 		output[j] = input & 0xff;
-	//	// 		output[j + 1] = (input >> 8) & 0xff;
-	//	// 		output[j + 2] = (input >> 16) & 0xff;
-	//	// 		output[j + 3] = (input >> 24) & 0xff;
-	//}
-
-	//return result;
-
 	return {
 		gsl::narrow_cast<uint8_t>((input >> 0) & 0xff),
 		gsl::narrow_cast<uint8_t>((input >> 8) & 0xff),
@@ -560,46 +269,9 @@ std::vector<uint8_t> TMD5::i_Encode( const std::uint64_t& input )
 		};
 }
 
-/* Decodes input (unsigned char) into output (UINT4). Assumes len is
-a multiple of 4.
-*/
-// void TMD5::_Decode (std::uint32_t *output, unsigned char const *input, uint32_t len)
-// {
-// 	uint32_t i, j;
-// 
-// 	for (i = 0, j = 0; j < len; i++, j += 4)
-// 		output[i] = (static_cast<std::uint32_t>(input[j])
-// 			| (static_cast<std::uint32_t>(input[j+1]) << 8) 
-// 			| (static_cast<std::uint32_t>(input[j+2]) << 16)
-// 			| (static_cast<std::uint32_t>(input[j+3]) << 24));
-// }
 
-/* Note: Replace "for loop" with standard memcpy if possible.
-*/
+//__________________________________________________________________________________________________
 
-//void TMD5::_MD5_memcpy( char* output, const char* input, uint32_t len )
-//{
-//	uint32_t i;
-//
-//	for (i = 0; i < len; i++)
-//	{
-//		output[i] = input[i];
-//	}
-//}
-
-/* Note: Replace "for loop" with standard memset if possible.
-*/
-// void TMD5::_MD5_memset (unsigned char *output, int value, uint32_t len)
-// {
-// 	uint32_t i;
-// 
-// 	for (i = 0; i < len; i++)
-// 		((char *)output)[i] = (char)value;
-// }
-
-
-//////////////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////////
 
 std::string Calc_MD5( const std::string& input )
 {
--- AoC_Solver_Engine/src/Utils/MD5/TMD5.hpp
+++ AoC_Solver_Engine/src/Utils/MD5/TMD5.hpp
@@ -2,11 +2,8 @@
 #ifndef TMD5_H_
 #define TMD5_H_
 
-//#include <istream>
-//#include <array>
 
 #include <vector>
-
 #include <string>
 
 
@@ -16,176 +13,34 @@ class TMD5
 public:
 
 	TMD5();
-	//	TMD5(std::istream const &pin);
 	explicit TMD5( const std::string& pin );
-	~TMD5();
+
 
 	//	TMD5 &operator +=( std::string const &pstr );
-
-	//char Value( const unsigned int pid ) const { return m_digest[pid]; }
 
 	std::string String() const;
 
 private:
 
-	//void _CalcMD5();
 
-	//void i_MD5Init();
-	//void _MD5Update(char const *, uint32_t);
 	void i_MD5Update( const std::vector<uint8_t>& ainput );
-	// 	void _MD5Update ( std::input_iterator_tag const &pstart
-	// 					, std::input_iterator_tag const &pend
-	// 					);
-	//	void _O_MD5Update (std::vector<char> const &pinput, uint32_t inputLen);
-
-	void i_MD5Update( std::vector<uint8_t>::const_iterator astart
-		, const std::vector<uint8_t>::const_iterator& aend );
-
-	// 	template <class TTInputIterator>
-	// 	void _Tmpl_MD5Update(TTInputIterator const &pstart, TTInputIterator const &pend);
+	void i_MD5Update( std::vector<uint8_t>::const_iterator astart, const std::vector<uint8_t>::const_iterator& aend );
 
 	void i_MD5Final();
 
-
-	//void _MD5Transform(uint32_t state[4], char const [64]);
-	//void _MD5Transform (uint32_t state[4], std::array<char, 64> block);
-	//void i_MD5Transform( const uint8_t block[64] );
 	void i_MD5Transform( const uint32_t ax[16] );
-	//void _MD5Transform (std::array<char, 64> block);
-	//void _Encode(char *, const std::uint32_t *, uint32_t);
-	std::vector<uint8_t> i_Encode( const std::uint64_t& input );
 
-	//	void _Decode(std::uint32_t *, unsigned char const *, uint32_t);
-	//void _MD5_memcpy( char*, const char*, uint32_t );
-	//	void _MD5_memset(unsigned char *, int, uint32_t);
+	std::vector<uint8_t> i_Encode( const std::uint64_t& input );
 
 private:
 
-	/* MD5 context. */
-	// 	struct MD5_CTX
-	// 	{
-	//uint32_t state[4];                                   /* state (ABCD) */
-	//uint32_t m_count[2];					/* number of bits, modulo 2^64 (lsb first) */
-	//uint64_t m_bitcount;					/* number of bits, modulo 2^64 (lsb first) */
+
 	uint64_t m_bytecount;
 
-	//char m_buffer[64];                         /* input buffer */
-
-
-	//union
-	//{
-	//	char m_block[64];
-	//	std::uint32_t m_xdata[16];
-	//};
-	//char m_block[64];
 	uint8_t m_Block[64];
-
-
-	//std::array<char, 64> m_buffer;
-														//};
-														//std::array<unsigned int, 32> m_MD5;
-	//std::array<unsigned int, 4> m_MD5;
-	//uint32_t m_MD5[4];
-	//unsigned char m_digest[16];
-	//unsigned char *m_digest;
-
-	//union
-	//{
-	//	uint32_t m_MD5[4];
-	//	unsigned char m_digest[16];
-	//	//unsigned char *m_digest;
-	//};
 
 	uint32_t m_MD5[4];
 	
-
-private:
-
-	//static char PADDING[64];	// = {
-	//static const std::vector<uint8_t> M_PADDING;
-	//static std::array<uint8_t, 64> PADDING;
-	// 		0x80, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-	// 		0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-	// 		0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
-	// 	};
-
-	
-	//static constexpr std::uint32_t S11 = 7;						//	#define S11 7
-	//static constexpr std::uint32_t S12 = 12;						//	#define S12 12
-	//static constexpr std::uint32_t S13 = 17;						//	#define S13 17
-	//static constexpr std::uint32_t S14 = 22;						//	#define S14 22
-	//static constexpr std::uint32_t S21 = 5;						//	#define S21 5
-	//static constexpr std::uint32_t S22 = 9;						//	#define S22 9
-	//static constexpr std::uint32_t S23 = 14;						//	#define S23 14
-	//static constexpr std::uint32_t S24 = 20;						//	#define S24 20
-	//static constexpr std::uint32_t S31 = 4;						//	#define S31 4
-	//static constexpr std::uint32_t S32 = 11;						//	#define S32 11
-	//static constexpr std::uint32_t S33 = 16;						//	#define S33 16
-	//static constexpr std::uint32_t S34 = 23;						//	#define S34 23
-	//static constexpr std::uint32_t S41 = 6;						//	#define S41 6
-	//static constexpr std::uint32_t S42 = 10;						//	#define S42 10
-	//static constexpr std::uint32_t S43 = 15;						//	#define S43 15
-	//static constexpr std::uint32_t S44 = 21;						//	#define S44 21
-
-	//static constexpr std::uint32_t _F( std::uint32_t x, std::uint32_t y, std::uint32_t z ) { return ((x & y) | ((~x) & z)); }
-	//static constexpr std::uint32_t _G( std::uint32_t x, std::uint32_t y, std::uint32_t z ) { return (x & z) | (y & (~z)); }
-	//static constexpr std::uint32_t _H( std::uint32_t x, std::uint32_t y, std::uint32_t z ) { return (x ^ y ^ z); }
-	//static constexpr std::uint32_t _I( std::uint32_t x, std::uint32_t y, std::uint32_t z ) { return (y ^ (x | (~z))); }
-
-	//// ROTATE_LEFT rotates x left n bits.
-	//static constexpr std::uint32_t _BitRotation_Left( std::uint32_t x, int n ) { return ((x << n) | (x >> (32 - n))); }
-	////std::uint32_t _BitRotation_Left(std::uint32_t x, int n)					{ return _rotl(x, n); }
-
-
-	//static constexpr std::uint32_t _FF( std::uint32_t a
-	//	, std::uint32_t b
-	//	, std::uint32_t c
-	//	, std::uint32_t d
-	//	, std::uint32_t x
-	//	, std::uint32_t s
-	//	, std::uint32_t ac
-	//)
-	//{
-	//	return _BitRotation_Left( a + _F( b, c, d ) + x + ac, s ) + b;
-	//}
-
-	//static constexpr std::uint32_t _GG( std::uint32_t a
-	//	, std::uint32_t b
-	//	, std::uint32_t c
-	//	, std::uint32_t d
-	//	, std::uint32_t x
-	//	, std::uint32_t s
-	//	, std::uint32_t ac
-	//)
-	//{
-	//	return _BitRotation_Left( a + _G( b, c, d ) + x + ac, s ) + b;
-	//}
-
-	//static constexpr std::uint32_t _HH( std::uint32_t a
-	//	, std::uint32_t b
-	//	, std::uint32_t c
-	//	, std::uint32_t d
-	//	, std::uint32_t x
-	//	, std::uint32_t s
-	//	, std::uint32_t ac
-	//)
-	//{
-	//	return _BitRotation_Left( a + _H( b, c, d ) + x + ac, s ) + b;
-	//}
-
-	//static constexpr std::uint32_t _II( std::uint32_t a
-	//	, std::uint32_t b
-	//	, std::uint32_t c
-	//	, std::uint32_t d
-	//	, std::uint32_t x
-	//	, std::uint32_t s
-	//	, std::uint32_t ac
-	//)
-	//{
-	//	return _BitRotation_Left( a + _I( b, c, d ) + x + ac, s ) + b;
-	//}
-
-
 };
 
 
@@ -193,5 +48,5 @@ std::string Calc_MD5( const std::string& input );
 
 
 
-#endif // TMD5_h__
+#endif // TMD5_H_
 
