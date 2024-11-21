171aa0f9e64ecdda6010b4f61852cf8cdb5d7f3b
JR <jr@mail.com>
2024-11-21 16:55:51 +0100

    Added commented lines of future solutions. - Code style.

--- AoC_Solver_Engine/src/2015/TAoC_15_Index.cpp
+++ AoC_Solver_Engine/src/2015/TAoC_15_Index.cpp
@@ -9,6 +9,24 @@
 #include "05/TAoCS_15_05.h"
 #include "06/TAoCS_15_06.h"
 #include "07/TAoCS_15_07.h"
+//#include "08/TAoCS_15_08.h"
+//#include "09/TAoCS_15_09.h"
+//#include "10/TAoCS_15_10.h"
+//#include "11/TAoCS_15_11.h"
+//#include "12/TAoCS_15_12.h"
+//#include "13/TAoCS_15_13.h"
+//#include "14/TAoCS_15_14.h"
+//#include "15/TAoCS_15_15.h"
+//#include "16/TAoCS_15_16.h"
+//#include "17/TAoCS_15_17.h"
+//#include "18/TAoCS_15_18.h"
+//#include "19/TAoCS_15_19.h"
+//#include "20/TAoCS_15_20.h"
+//#include "21/TAoCS_15_21.h"
+//#include "22/TAoCS_15_22.h"
+//#include "23/TAoCS_15_23.h"
+//#include "24/TAoCS_15_24.h"
+//#include "25/TAoCS_15_25.h"
 
 
 namespace y15
@@ -23,13 +41,33 @@ std::unique_ptr<TAoC_Solver> Get_Solver( const TPuzzleID& id )
 
 	switch (id.Day)
 	{
-	case 1: return y15::d01::Get_Solver( id.Part );
-	case 2: return y15::d02::Get_Solver( id.Part );
-	case 3: return y15::d03::Get_Solver( id.Part );
-	case 4: return y15::d04::Get_Solver( id.Part );
-	case 5: return y15::d05::Get_Solver( id.Part );
-	case 6: return y15::d06::Get_Solver( id.Part );
-	case 7: return y15::d07::Get_Solver( id.Part );
+	case  1: return y15::d01::Get_Solver( id.Part );
+	case  2: return y15::d02::Get_Solver( id.Part );
+	case  3: return y15::d03::Get_Solver( id.Part );
+	case  4: return y15::d04::Get_Solver( id.Part );
+	case  5: return y15::d05::Get_Solver( id.Part );
+	case  6: return y15::d06::Get_Solver( id.Part );
+	case  7: return y15::d07::Get_Solver( id.Part );
+	//case  8: return y15::d08::Get_Solver( id.Part );
+	//case  9: return y15::d09::Get_Solver( id.Part );
+	//case 10: return y15::d10::Get_Solver( id.Part );
+	//case 11: return y15::d11::Get_Solver( id.Part );
+	//case 12: return y15::d12::Get_Solver( id.Part );
+	//case 13: return y15::d13::Get_Solver( id.Part );
+	//case 14: return y15::d14::Get_Solver( id.Part );
+	//case 15: return y15::d15::Get_Solver( id.Part );
+	//case 16: return y15::d16::Get_Solver( id.Part );
+	//case 17: return y15::d17::Get_Solver( id.Part );
+	//case 18: return y15::d18::Get_Solver( id.Part );
+	//case 19: return y15::d19::Get_Solver( id.Part );
+	//case 20: return y15::d20::Get_Solver( id.Part );
+	//case 21: return y15::d21::Get_Solver( id.Part );
+	//case 22: return y15::d22::Get_Solver( id.Part );
+	//case 23: return y15::d23::Get_Solver( id.Part );
+	//case 24: return y15::d24::Get_Solver( id.Part );
+	//case 25: return y15::d25::Get_Solver( id.Part );
+
+	
 
 	default: return std::make_unique<TAoC_Solver_NULL>();
 	}
--- AoC_Solver_Engine/src/Utils/MD5/JMD5.cpp
+++ AoC_Solver_Engine/src/Utils/MD5/JMD5.cpp
@@ -158,76 +158,76 @@ void TSessionMD5::i_MD5Transform( const std::span<const uint32_t, 16>& ax ) noex
 
 
 	/* Round 1 */
-	a = _FF( a, b, c, d, ax[0], S11, 0xd76aa478 ); /* 1 */
-	d = _FF( d, a, b, c, ax[1], S12, 0xe8c7b756 ); /* 2 */
-	c = _FF( c, d, a, b, ax[2], S13, 0x242070db ); /* 3 */
-	b = _FF( b, c, d, a, ax[3], S14, 0xc1bdceee ); /* 4 */
-	a = _FF( a, b, c, d, ax[4], S11, 0xf57c0faf ); /* 5 */
-	d = _FF( d, a, b, c, ax[5], S12, 0x4787c62a ); /* 6 */
-	c = _FF( c, d, a, b, ax[6], S13, 0xa8304613 ); /* 7 */
-	b = _FF( b, c, d, a, ax[7], S14, 0xfd469501 ); /* 8 */
-	a = _FF( a, b, c, d, ax[8], S11, 0x698098d8 ); /* 9 */
-	d = _FF( d, a, b, c, ax[9], S12, 0x8b44f7af ); /* 10 */
-	c = _FF( c, d, a, b, ax[10], S13, 0xffff5bb1 ); /* 11 */
-	b = _FF( b, c, d, a, ax[11], S14, 0x895cd7be ); /* 12 */
-	a = _FF( a, b, c, d, ax[12], S11, 0x6b901122 ); /* 13 */
-	d = _FF( d, a, b, c, ax[13], S12, 0xfd987193 ); /* 14 */
-	c = _FF( c, d, a, b, ax[14], S13, 0xa679438e ); /* 15 */
-	b = _FF( b, c, d, a, ax[15], S14, 0x49b40821 ); /* 16 */
+	a = _FF( a, b, c, d, ax[ 0], S11, 0xd76aa478 ); //  1
+	d = _FF( d, a, b, c, ax[ 1], S12, 0xe8c7b756 ); //  2
+	c = _FF( c, d, a, b, ax[ 2], S13, 0x242070db ); //  3
+	b = _FF( b, c, d, a, ax[ 3], S14, 0xc1bdceee ); //  4
+	a = _FF( a, b, c, d, ax[ 4], S11, 0xf57c0faf ); //  5
+	d = _FF( d, a, b, c, ax[ 5], S12, 0x4787c62a ); //  6
+	c = _FF( c, d, a, b, ax[ 6], S13, 0xa8304613 ); //  7
+	b = _FF( b, c, d, a, ax[ 7], S14, 0xfd469501 ); //  8
+	a = _FF( a, b, c, d, ax[ 8], S11, 0x698098d8 ); //  9
+	d = _FF( d, a, b, c, ax[ 9], S12, 0x8b44f7af ); // 10
+	c = _FF( c, d, a, b, ax[10], S13, 0xffff5bb1 ); // 11
+	b = _FF( b, c, d, a, ax[11], S14, 0x895cd7be ); // 12
+	a = _FF( a, b, c, d, ax[12], S11, 0x6b901122 ); // 13
+	d = _FF( d, a, b, c, ax[13], S12, 0xfd987193 ); // 14
+	c = _FF( c, d, a, b, ax[14], S13, 0xa679438e ); // 15
+	b = _FF( b, c, d, a, ax[15], S14, 0x49b40821 ); // 16
 
 	/* Round 2 */
-	a = _GG( a, b, c, d, ax[1], S21, 0xf61e2562 ); /* 17 */
-	d = _GG( d, a, b, c, ax[6], S22, 0xc040b340 ); /* 18 */
-	c = _GG( c, d, a, b, ax[11], S23, 0x265e5a51 ); /* 19 */
-	b = _GG( b, c, d, a, ax[0], S24, 0xe9b6c7aa ); /* 20 */
-	a = _GG( a, b, c, d, ax[5], S21, 0xd62f105d ); /* 21 */
-	d = _GG( d, a, b, c, ax[10], S22, 0x2441453 ); /* 22 */
-	c = _GG( c, d, a, b, ax[15], S23, 0xd8a1e681 ); /* 23 */
-	b = _GG( b, c, d, a, ax[4], S24, 0xe7d3fbc8 ); /* 24 */
-	a = _GG( a, b, c, d, ax[9], S21, 0x21e1cde6 ); /* 25 */
-	d = _GG( d, a, b, c, ax[14], S22, 0xc33707d6 ); /* 26 */
-	c = _GG( c, d, a, b, ax[3], S23, 0xf4d50d87 ); /* 27 */
-	b = _GG( b, c, d, a, ax[8], S24, 0x455a14ed ); /* 28 */
-	a = _GG( a, b, c, d, ax[13], S21, 0xa9e3e905 ); /* 29 */
-	d = _GG( d, a, b, c, ax[2], S22, 0xfcefa3f8 ); /* 30 */
-	c = _GG( c, d, a, b, ax[7], S23, 0x676f02d9 ); /* 31 */
-	b = _GG( b, c, d, a, ax[12], S24, 0x8d2a4c8a ); /* 32 */
+	a = _GG( a, b, c, d, ax[ 1], S21, 0xf61e2562 ); // 17
+	d = _GG( d, a, b, c, ax[ 6], S22, 0xc040b340 ); // 18
+	c = _GG( c, d, a, b, ax[11], S23, 0x265e5a51 ); // 19
+	b = _GG( b, c, d, a, ax[ 0], S24, 0xe9b6c7aa ); // 20
+	a = _GG( a, b, c, d, ax[ 5], S21, 0xd62f105d ); // 21
+	d = _GG( d, a, b, c, ax[10], S22, 0x02441453 ); // 22
+	c = _GG( c, d, a, b, ax[15], S23, 0xd8a1e681 ); // 23
+	b = _GG( b, c, d, a, ax[ 4], S24, 0xe7d3fbc8 ); // 24
+	a = _GG( a, b, c, d, ax[ 9], S21, 0x21e1cde6 ); // 25
+	d = _GG( d, a, b, c, ax[14], S22, 0xc33707d6 ); // 26
+	c = _GG( c, d, a, b, ax[ 3], S23, 0xf4d50d87 ); // 27
+	b = _GG( b, c, d, a, ax[ 8], S24, 0x455a14ed ); // 28
+	a = _GG( a, b, c, d, ax[13], S21, 0xa9e3e905 ); // 29
+	d = _GG( d, a, b, c, ax[ 2], S22, 0xfcefa3f8 ); // 30
+	c = _GG( c, d, a, b, ax[ 7], S23, 0x676f02d9 ); // 31
+	b = _GG( b, c, d, a, ax[12], S24, 0x8d2a4c8a ); // 32
 
 	/* Round 3 */
-	a = _HH( a, b, c, d, ax[5], S31, 0xfffa3942 ); /* 33 */
-	d = _HH( d, a, b, c, ax[8], S32, 0x8771f681 ); /* 34 */
-	c = _HH( c, d, a, b, ax[11], S33, 0x6d9d6122 ); /* 35 */
-	b = _HH( b, c, d, a, ax[14], S34, 0xfde5380c ); /* 36 */
-	a = _HH( a, b, c, d, ax[1], S31, 0xa4beea44 ); /* 37 */
-	d = _HH( d, a, b, c, ax[4], S32, 0x4bdecfa9 ); /* 38 */
-	c = _HH( c, d, a, b, ax[7], S33, 0xf6bb4b60 ); /* 39 */
-	b = _HH( b, c, d, a, ax[10], S34, 0xbebfbc70 ); /* 40 */
-	a = _HH( a, b, c, d, ax[13], S31, 0x289b7ec6 ); /* 41 */
-	d = _HH( d, a, b, c, ax[0], S32, 0xeaa127fa ); /* 42 */
-	c = _HH( c, d, a, b, ax[3], S33, 0xd4ef3085 ); /* 43 */
-	b = _HH( b, c, d, a, ax[6], S34, 0x4881d05 ); /* 44 */
-	a = _HH( a, b, c, d, ax[9], S31, 0xd9d4d039 ); /* 45 */
-	d = _HH( d, a, b, c, ax[12], S32, 0xe6db99e5 ); /* 46 */
-	c = _HH( c, d, a, b, ax[15], S33, 0x1fa27cf8 ); /* 47 */
-	b = _HH( b, c, d, a, ax[2], S34, 0xc4ac5665 ); /* 48 */
+	a = _HH( a, b, c, d, ax[ 5], S31, 0xfffa3942 ); // 33
+	d = _HH( d, a, b, c, ax[ 8], S32, 0x8771f681 ); // 34
+	c = _HH( c, d, a, b, ax[11], S33, 0x6d9d6122 ); // 35
+	b = _HH( b, c, d, a, ax[14], S34, 0xfde5380c ); // 36
+	a = _HH( a, b, c, d, ax[ 1], S31, 0xa4beea44 ); // 37
+	d = _HH( d, a, b, c, ax[ 4], S32, 0x4bdecfa9 ); // 38
+	c = _HH( c, d, a, b, ax[ 7], S33, 0xf6bb4b60 ); // 39
+	b = _HH( b, c, d, a, ax[10], S34, 0xbebfbc70 ); // 40
+	a = _HH( a, b, c, d, ax[13], S31, 0x289b7ec6 ); // 41
+	d = _HH( d, a, b, c, ax[ 0], S32, 0xeaa127fa ); // 42
+	c = _HH( c, d, a, b, ax[ 3], S33, 0xd4ef3085 ); // 43
+	b = _HH( b, c, d, a, ax[ 6], S34, 0x04881d05 ); // 44
+	a = _HH( a, b, c, d, ax[ 9], S31, 0xd9d4d039 ); // 45
+	d = _HH( d, a, b, c, ax[12], S32, 0xe6db99e5 ); // 46
+	c = _HH( c, d, a, b, ax[15], S33, 0x1fa27cf8 ); // 47
+	b = _HH( b, c, d, a, ax[ 2], S34, 0xc4ac5665 ); // 48
 
 	/* Round 4 */
-	a = _II( a, b, c, d, ax[0], S41, 0xf4292244 ); /* 49 */
-	d = _II( d, a, b, c, ax[7], S42, 0x432aff97 ); /* 50 */
-	c = _II( c, d, a, b, ax[14], S43, 0xab9423a7 ); /* 51 */
-	b = _II( b, c, d, a, ax[5], S44, 0xfc93a039 ); /* 52 */
-	a = _II( a, b, c, d, ax[12], S41, 0x655b59c3 ); /* 53 */
-	d = _II( d, a, b, c, ax[3], S42, 0x8f0ccc92 ); /* 54 */
-	c = _II( c, d, a, b, ax[10], S43, 0xffeff47d ); /* 55 */
-	b = _II( b, c, d, a, ax[1], S44, 0x85845dd1 ); /* 56 */
-	a = _II( a, b, c, d, ax[8], S41, 0x6fa87e4f ); /* 57 */
-	d = _II( d, a, b, c, ax[15], S42, 0xfe2ce6e0 ); /* 58 */
-	c = _II( c, d, a, b, ax[6], S43, 0xa3014314 ); /* 59 */
-	b = _II( b, c, d, a, ax[13], S44, 0x4e0811a1 ); /* 60 */
-	a = _II( a, b, c, d, ax[4], S41, 0xf7537e82 ); /* 61 */
-	d = _II( d, a, b, c, ax[11], S42, 0xbd3af235 ); /* 62 */
-	c = _II( c, d, a, b, ax[2], S43, 0x2ad7d2bb ); /* 63 */
-	b = _II( b, c, d, a, ax[9], S44, 0xeb86d391 ); /* 64 */
+	a = _II( a, b, c, d, ax[ 0], S41, 0xf4292244 ); // 49
+	d = _II( d, a, b, c, ax[ 7], S42, 0x432aff97 ); // 50
+	c = _II( c, d, a, b, ax[14], S43, 0xab9423a7 ); // 51
+	b = _II( b, c, d, a, ax[ 5], S44, 0xfc93a039 ); // 52
+	a = _II( a, b, c, d, ax[12], S41, 0x655b59c3 ); // 53
+	d = _II( d, a, b, c, ax[ 3], S42, 0x8f0ccc92 ); // 54
+	c = _II( c, d, a, b, ax[10], S43, 0xffeff47d ); // 55
+	b = _II( b, c, d, a, ax[ 1], S44, 0x85845dd1 ); // 56
+	a = _II( a, b, c, d, ax[ 8], S41, 0x6fa87e4f ); // 57
+	d = _II( d, a, b, c, ax[15], S42, 0xfe2ce6e0 ); // 58
+	c = _II( c, d, a, b, ax[ 6], S43, 0xa3014314 ); // 59
+	b = _II( b, c, d, a, ax[13], S44, 0x4e0811a1 ); // 60
+	a = _II( a, b, c, d, ax[ 4], S41, 0xf7537e82 ); // 61
+	d = _II( d, a, b, c, ax[11], S42, 0xbd3af235 ); // 62
+	c = _II( c, d, a, b, ax[ 2], S43, 0x2ad7d2bb ); // 63
+	b = _II( b, c, d, a, ax[ 9], S44, 0xeb86d391 ); // 64
 
 	m_MD5[0] += a;
 	m_MD5[1] += b;
@@ -241,8 +241,8 @@ void TSessionMD5::i_MD5Transform( const std::span<const uint32_t, 16>& ax ) noex
 std::array<std::byte, 8> TSessionMD5::i_Encode( const std::uint64_t& input ) noexcept
 {
 	return {
-		gsl::narrow_cast<std::byte>((input >> 0) & 0xff),
-		gsl::narrow_cast<std::byte>((input >> 8) & 0xff),
+		gsl::narrow_cast<std::byte>((input >>  0) & 0xff),
+		gsl::narrow_cast<std::byte>((input >>  8) & 0xff),
 		gsl::narrow_cast<std::byte>((input >> 16) & 0xff),
 		gsl::narrow_cast<std::byte>((input >> 24) & 0xff),
 		gsl::narrow_cast<std::byte>((input >> 32) & 0xff),
@@ -262,11 +262,11 @@ std::string TSessionMD5::i_MD5_to_string() const
 	int pos = 0;
 	for (auto i = 0u; i < 4u; ++i)
 	{
-		result[pos++] = lookup[(m_MD5[i] >> 4) & 0x0F];
-		result[pos++] = lookup[m_MD5[i] & 0x0F];
+		result[pos++] = lookup[(m_MD5[i] >>  4) & 0x0F];
+		result[pos++] = lookup[ m_MD5[i]        & 0x0F];
 
 		result[pos++] = lookup[(m_MD5[i] >> 12) & 0x0F];
-		result[pos++] = lookup[(m_MD5[i] >> 8) & 0x0F];
+		result[pos++] = lookup[(m_MD5[i] >>  8) & 0x0F];
 
 		result[pos++] = lookup[(m_MD5[i] >> 20) & 0x0F];
 		result[pos++] = lookup[(m_MD5[i] >> 16) & 0x0F];
